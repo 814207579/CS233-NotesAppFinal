5737212b10510e55562d6b99cfdf5450f34d5a63
Caleb M Breaux <814196499@wwcc.edu>
2019-12-09 10:53:26 -0800

    Add project files.

---
+++ Models/Note.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotesAppFinal.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string Heading { get; set; }
+        public string Content { get; set; }
+    }
+}
